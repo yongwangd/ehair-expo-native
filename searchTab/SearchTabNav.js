6c5624d2b80c136b705380533c1331c7fa62e333
Yong Wang <yongwangd@gmail.com>
2018-03-09 16:59:41 -0500

    3

--- searchTab/SearchTabNav.js
+++ searchTab/SearchTabNav.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { StackNavigator } from 'react-navigation';
-import { Text, View } from 'react-native';
-import SearchScreen from './SearchScreen';
+import { GRAY, WHITE } from '../lib/colors';
+import { Image, Text, View } from 'react-native';
+
 import ContactDetailScreen from './ContactDetailScreen';
+import React from 'react';
 import SearchBoxContainer from '../containers/SearchBoxContainer';
-import { WHITE } from '../lib/colors';
+import SearchScreen from './SearchScreen';
+import { StackNavigator } from 'react-navigation';
 
 export default StackNavigator({
   SearchScreen: {
@@ -12,7 +13,18 @@ export default StackNavigator({
     navigationOptions: ({ navigation }) => ({
       title: 'Search Products',
       header: (
-        <View style={{ paddingTop: 25, backgroundColor: WHITE }}>
+        <View
+          style={{
+            paddingTop: 25,
+            backgroundColor: GRAY,
+            display: 'flex'
+          }}
+        >
+          {/*           <Image
+            source={require('../assets/images/ehair-logo.png')}
+            style={{ width: 40, height: 20 }}
+            resizeMode="contain"
+/> */}
           <SearchBoxContainer />
         </View>
       )
