c10439b0e2368eb2d892f1a597cdad239d55c51c
Yong Wang <yongwangd@gmail.com>
2018-03-14 16:06:07 -0400

    3

--- homeTab/TagContactsScreen.js
+++ homeTab/TagContactsScreen.js
@@ -4,19 +4,11 @@ import ContactListContainer from '../searchTab/ContactListContainer';
 import { contactsSelector } from '../selectors/contactSelectors';
 
 class TagContactsScreen extends React.Component {
-//   onContactClick = contact => {
-//     this.props.navigation.navigate('ContactDetailScreen', {
-//       contactId: contact._id,
-//       title: contact.name
-//     });
-//   };
-
   render() {
     console.log('render of tag contacts', this.props);
     return (
       <ContactListContainer
         style={{ flex: 1, display: 'flex' }}
-        // onContactClick={this.onContactClick}
         contacts={this.props.contacts}
       />
     );
--- homeTab/WatchListScreen.js
+++ homeTab/WatchListScreen.js
@@ -4,19 +4,11 @@ import ContactListContainer from '../searchTab/ContactListContainer';
 import { watchListSelector } from '../selectors/contactSelectors';
 
 class WatchListScreen extends React.Component {
-  onContactClick = contact => {
-    this.props.navigation.navigate('ContactDetailScreen', {
-      contactId: contact._id,
-      title: contact.name
-    });
-  };
-
   render() {
     return (
       <ContactListContainer
         style={{ flex: 1, display: 'flex' }}
         contacts={this.props.contacts}
-        onContactClick={this.onContactClick}
       />
     );
   }
